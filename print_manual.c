754b5d6938ecff6dd72f9f0b38ecf0dccb5ea0cb
navya7093 <navyakontala@gmail.com>
2024-07-26 22:07:41 +0530

    Update print_manual.c

--- print_manual.c
+++ print_manual.c
@@ -1,34 +1,6 @@
-#include <stdio.h>
 #include "color_coding.h"
 
-#define MAX_COLORPAIR_NAME_CHARS 16
-
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
-{
-    
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
-  
-}
-
-
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
-{
-   
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
-   
+int main() {
+    printColorCodingManual();
+    return 0;
 }
