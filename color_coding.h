c4c5f64d5452e991b07c26969db9109cd9639ccf
navya7093 <navyakontala@gmail.com>
2024-07-26 20:13:45 +0530

    Create color_coding.h

---
+++ color_coding.h
@@ -0,0 +1,21 @@
+#ifndef COLOR_CODING_H
+#define COLOR_CODING_H
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+
+#endif // COLOR_CODING_H
