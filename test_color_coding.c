e9468841672368e16cef43f4a89e87ddc998d631
navya7093 <navyakontala@gmail.com>
2024-07-26 22:07:07 +0530

    Update test_color_coding.c

--- test_color_coding.c
+++ test_color_coding.c
@@ -4,37 +4,34 @@
 
 #define MAX_COLORPAIR_NAME_CHARS 16
 
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
-{
+void checkColorPair(const ColorPair* colorPair, enum MajorColor expectedMajor, enum MinorColor expectedMinor) {
+    assert(colorPair->majorColor == expectedMajor);
+    assert(colorPair->minorColor == expectedMinor);
+}
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor) {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
     ColorPairToString(&colorPair, colorPairNames);
     printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
+    checkColorPair(&colorPair, expectedMajor, expectedMinor);
 }
 
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
-{
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber) {
+    ColorPair colorPair = {major, minor};
     int pairNumber = GetPairNumberFromColor(&colorPair);
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
 }
 
-int main() {
+void runTests() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
-
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+}
 
+int main() {
+    runTests();
     return 0;
 }
