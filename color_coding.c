49672b51125f2b4dccfffd23dc0b646af2661454
navya7093 <navyakontala@gmail.com>
2024-07-26 22:06:49 +0530

    Update color_coding.c

--- color_coding.c
+++ color_coding.c
@@ -1,11 +1,44 @@
+#include <stdio.h>
 #include "color_coding.h"
-int main() {
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
 
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
-     printf("\nColor Coding Reference Manual:\n");
-    PrintColorCodingReferenceManual();
-    return 0;
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
+
+void printColorCodingManual() {
+    for (int i = 1; i <= numberOfMajorColors * numberOfMinorColors; i++) {
+        ColorPair colorPair = GetColorFromPairNumber(i);
+        char colorPairNames[16];
+        ColorPairToString(&colorPair, colorPairNames);
+        printf("%d: %s\n", i, colorPairNames);
+    }
 }
