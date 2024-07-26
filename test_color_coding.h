d81d2412d9ea5f394269740d647169d64ba6e5b6
navya7093 <navyakontala@gmail.com>
2024-07-26 22:42:24 +0530

    Update test_color_coding.h

--- test_color_coding.h
+++ test_color_coding.h
@@ -1,6 +1,6 @@
 #ifndef TEST_COLOR_CODING_H
 #define TEST_COLOR_CODING_H
-#include "ColorPair.h"
+#include "color_coding.h"
 
 void colorPairToString(const ColorPair* colorPair, char* buffer);
 ColorPair getColorFromPairNumber(int pairNumber);
